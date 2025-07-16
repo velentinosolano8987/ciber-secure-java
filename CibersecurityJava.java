5c853adcea792df68fde10a69b43625335a4f891
velentinosolano8987 <vduttosolano020@materpurissima.edu.ar>
2025-07-16 16:19:19 -0300

    Add files via upload

---
+++ CibersecurityJava.java
@@ -0,0 +1,52 @@
+package com.mycompany.cibersecurity.java;
+
+import javax.accessibility.Accessible;
+import java.time.LocalDateTime;
+
+@path
+@Entity
+@Setter
+@Getter
+@Table(name = 'user')
+@NoArgsConstructor
+@AllArgsConstructor
+
+public classUser;{
+
+@id
+@GeneratedValue(Strategy = GenerationType.IDENTITY)
+
+private long id;
+
+@Column(nullable = false,unique = true)
+
+private String username;
+
+@Column(nullable = false)
+private String password;
+
+@Colunm(name = 'last_login')
+private localDateTime LastLogin;
+
+@column(nullable = false)
+private String role ='role_user'; 
+}
+}
+private Class files{
+if(Catch:
+file.File_wrong = Delete.wrong_file);
+
+private Class sql_Public{
+writePrivate = sql.get_id;
+private sql.getRegion_id;
+private sql.getName;
+private sql.account_id;
+private sql.get_ip;
+
+private sql.write = new writeSql(path);
+
+}
+return null;
+}
+}
+//eliminate the wrong in the line 10
